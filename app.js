44928c62ede989727c2d79eb7c6bf70492548c17
Angel Lopez <angellopezixcayau@gmail.com>
2024-10-22 23:07:51 -0600

    mejoras en el header

--- app.js
+++ app.js
@@ -62,30 +62,43 @@ projects.forEach((project) => {
     container.appendChild(projectBox);
 });
 
-// // Menu Hamburguesa
-// const nav = document.querySelector("#navVisible");
-// const btnOpen = document.querySelector("#btnOpen");
-// const btnClose = document.querySelector("#btnClose");
+// Menu Hamburguesa
+const nav = document.querySelector("#navVisible");
+const btnOpen = document.querySelector("#btnOpen");
+const btnClose = document.querySelector("#btnClose");
+const sections = [
+  document.querySelector('#hero'),
+  document.querySelector('#aboutMe'),
+  document.querySelector('#experience'),
+  document.querySelector('#projects'),
+  document.querySelector('#skills'),
+  document.querySelector('#contact')
+];
 
-// btnOpen.addEventListener("click", () => {
-//   nav.classList.add("ul-visible");
-// });
 
-// btnClose.addEventListener("click", () => {
-//   nav.classList.remove("ul-visible");
-// });
+btnOpen.addEventListener("click", () => {
+  nav.classList.add("ul-visible");
+});
 
-// //Menu de filtros
-// const menuFilter = document.querySelector("#menuFilter");
-// const btnOpenMenu = document.querySelector("#btnOpenMenu");
+btnClose.addEventListener("click", () => {
+  nav.classList.remove("ul-visible");
+});
 
-// btnOpenMenu.addEventListener("click", () => {
-//   // Si el menú ya está visible
-//   if (menuFilter.classList.contains("menu-visible")) {
-//     // Si está visible, lo ocultamos quitando la clase
-//     menuFilter.classList.remove("menu-visible");
-//   } else {
-//     // Si no está visible, lo mostramos agregando la clase
-//     menuFilter.classList.add("menu-visible");
-//   }
-// });
+// Obtener todos los enlaces en el menú
+const anchors = nav.querySelectorAll('a');
+
+sections.forEach(section => {
+  section.addEventListener("click", () => {
+    nav.classList.remove("ul-visible");
+  });
+});
+
+anchors.forEach(anchor => {
+  anchor.addEventListener("click", () => {
+    nav.classList.remove("ul-visible");
+  });
+});
+
+
+
+
